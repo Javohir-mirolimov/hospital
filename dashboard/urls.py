63be26f8d4ef3d82ef7742e370ae37bcd223456e
Javohir-mirolimov <githup@gmail.com>
2024-02-18 20:05:37 +0500

    javoxir

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-
+from .views import *
 
 urlpatterns=[
+    #--------------Start  Curd  Employee Model ----------------
+    path('get-employee/', GetEmployee.as_view()),
+    path('create-employee/', CreateEmployee.as_view()),
+    path('update-employee/<int:pk>/', UpdateEmployee.as_view()),
+    path('delete-employee/<int:pk>/', DeleteEmployee.as_view()),
 
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, ListCreateAPIView, UpdateAPIView, DestroyAPIView, GenericAPIView
+from main.models import *
+from main.serializers import *
 
-# Create your views here.
+
+""" Start Crud Model """
+
+
+#<<<<<<<<<<<<<<< Start Curd Employee>>>>>>>>>>>>>>>>>
+class GetEmployee(ListAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializres
+
+class CreateEmployee(ListCreateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializres
+
+
+class UpdateEmployee(UpdateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializres
+
+class DeleteEmployee(DestroyAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializres
+
+#<<<<<<<<<<<<<< End Curd Employee>>>>>>>>>>>>>>>>>
+
+#<<<<<<<<<<<<<< Start
+
+""" End Crud Model """
+
