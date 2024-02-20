b266bed2ed9f38563ac789b70d80c13b0405fd58
Javohir-mirolimov <githup@gmail.com>
2024-02-20 12:46:22 +0500

    javoxir

--- account/views.py
+++ account/views.py
@@ -1,3 +1,171 @@
 from django.shortcuts import render
+from main.models import User
+from main.serializers import UserSerializers
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from django.contrib.auth import login, logout, authenticate
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+
+
+
+
+@api_view(['POST'])
+def signin_view(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    try:
+        usr = authenticate(username=username, password=password)
+        try:
+            if usr is not None:
+                login(request, usr)
+                tokens = get_tokens_for_user(usr)
+                status = 200
+                data = {
+                    'status': status,
+                    'username': username,
+                    'token': tokens,
+                }
+            else:
+                status = 403
+                message = "Invalid Password or Username!"
+                data = {
+                    'status': status,
+                    'message': message,
+                }
+        except User.DoesNotExist:
+            status = 404
+            message = 'This User is not defined! '
+            data = {
+                'status': status,
+                'message': message,
+            }
+        return Response(data)
+    except Exception as err:
+        return Response(f'{err}')
+
+
+@api_view(['POST'])
+def singup_view(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    phone_number = request.POST.get('phone_number')
+    avatar = request.FILES.get('avatar')
+    age = region.POST.get('age')
+    state = region.POST.get('state')
+    region = region.POST.get('region')
+    addres_street = region.POST.get('addres_street')
+    sex = region.POST.get('sex')
+    nationality = region.POST.get('nationality')
+    password_series = region.POST.get('password_series')
+    status = region.POST.get('status')
+    new = User.objects.create_user(
+        username=username,
+        password=password,
+        phone_number=phone_number,
+        age = age,
+        state=state,
+        region =region,
+        sex =sex,
+        nationality =nationality,
+        password_series =password_series,
+        status = status,
+    )
+    ser = UserSerializres(new)
+    return Response(ser.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def logout_view(request):
+    logout(request)
+    return Response({'data': 'sucses'})
+
+
+@api_view(['PUT'])
+def edit_user_view(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+        try:
+            username = request.data.get('username')
+            first_name = request.data.get('first_name')
+            last_name = request.data.get('last_name')
+            email = request.data.get('email')
+            address = request.data.get('address')
+            phone_number = request.data.get('phone_number')
+            password = request.data.get('password')
+            age = region.data.get('age')
+            state = region.data.get('state')
+            region = region.data.get('region')
+            addres_street = region.data.get('addres_street')
+            sex = region.data.get('sex')
+            nationality = region.data.get('nationality')
+            password_series = region.data.get('password_series')
+            status = region.data.get('status')
+            if username is not None:
+                user.username = username
+            if first_name is not None:
+                user.first_name = first_name
+            if last_name is not None:
+                user.last_name = last_name
+            if email is not None:
+                user.email = email
+            if address is not None:
+                user.address_id = address
+            if phone_number is not None:
+                user.phone_number = phone_number
+            if password is not None:
+                user.set_password(password)
+            if age is not None:
+                user.age = age
+            if state is not None:
+                user.state = state
+            if region is not None:
+                user.region = region
+            if addres_street is not None:
+                user.addres_street = addres_street
+            if sex is not None:
+                user.sex = sex
+            if nationality is not None:
+                user.nationality = nationality
+            if password_series is not  None:
+                user.password_series = password_series
+            if status is not None:
+                user.status = status
+            user.save()
+            ser = UserSerializer(user)
+            return Response(ser.data)
+        except:
+            status = 404
+            message = "Request failed"
+            data = {
+                'status': status,
+                'message': message,
+            }
+    except:
+        status = 404
+        message = "User not found"
+        data = {
+            'status': status,
+            'message': message
+        }
+    return Response(data)
+
+
+@api_view(['DELETE'])
+def delete_user(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+        user.delete()
+        data = {
+            'message': "User deleted successfully"
+        }
+    except:
+        status = 404
+        message = "User not found"
+        data = {
+            'status': status,
+            'message': message
+        }
+    return Response(data)
--- dashboard/views.py
+++ dashboard/views.py
@@ -10,99 +10,98 @@ from main.serializers import *
 #<<<<<<<<<<<<<<< Start Crud Employee>>>>>>>>>>>>>>>>>
 class GetEmployee(ListAPIView):
     queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializres
-
+    serializer_class = EmployeeSerializers
 class CreateEmployee(ListCreateAPIView):
     queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializres
+    serializer_class = EmployeeSerializers
 
 
 class UpdateEmployee(UpdateAPIView):
     queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializres
+    serializer_class = EmployeeSerializers
 
 class DeleteEmployee(DestroyAPIView):
     queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializres
+    serializer_class = EmployeeSerializers
 
 #<<<<<<<<<<<<<< End Crud Employee>>>>>>>>>>>>>>>>>
 
 #<<<<<<<<<<<<<< Start Crud Revenue>>>>>>>>>>>>>>>>
 class GetRevenue(ListAPIView):
     queryset = Revenue.objects.all()
-    serializer_class = RevenueSerializres
+    serializer_class = RevenueSerializers
 
 class CreateRevenue(ListCreateAPIView):
     queryset = Revenue.objects.all()
-    serializer_class = RevenueSerializres
+    serializer_class = RevenueSerializers
 
 
 class UpdateRevenue(UpdateAPIView):
     queryset = Revenue.objects.all()
-    serializer_class = RevenueSerializres
+    serializer_class = RevenueSerializers
 
 class DeleteRevenue(DestroyAPIView):
     queryset = Revenue.objects.all()
-    serializer_class = Revenue
+    serializer_class = RevenueSerializers
 
 #<<<<<<<<<<<<<< End Crud Revenue>>>>>>>>>>>>>>>>
 
 #<<<<<<<<<<<<<< Start Crud Payment>>>>>>>>>>>>>>
 class GetPayment(ListAPIView):
     queryset = Payment.objects.all()
-    serializer_class = PaymentSerializres
+    serializer_class = PaymentSerializers
 
 class CreatePayment(ListCreateAPIView):
     queryset = Payment.objects.all()
-    serializer_class = PaymentSerializres
+    serializer_class = PaymentSerializers
 
 class UpdatePayment(UpdateAPIView):
     queryset = Payment.objects.all()
-    serializer_class = PaymentSerializres
+    serializer_class = PaymentSerializers
 
 class DeletePayment(DestroyAPIView):
     queryset = Payment.objects.all()
-    serializer_class = PaymentSerializres
+    serializer_class = PaymentSerializers
 
 #<<<<<<<<<<<<<< End Crud Payment>>>>>>>>>>>>>>
 
 #<<<<<<<<<<<<<<Start Curd Testimonial_patient>>>>>>>>>>>
 class GetTestimonial_patient(ListAPIView):
     queryset = Testimonial_patient.objects.all()
-    serializer_class = Testimonial_patientSerializres
+    serializer_class = Testimonial_patientSerializers
 
 
 class CreateTestimonial_patient(ListCreateAPIView):
     queryset = Testimonial_patient.objects.all()
-    serializer_class = Testimonial_patient
+    serializer_class = Testimonial_patientSerializers
 
 
 class UpdateTestimonial_patient(UpdateAPIView):
     queryset = Testimonial_patient.objects.all()
-    serializer_class = Testimonial_patient
+    serializer_class = Testimonial_patientSerializers
 
 class DeleteTestimonial_patient(DestroyAPIView):
     queryset = Testimonial_patient.objects.all()
-    serializer_class = Testimonial_patientSerializres
+    serializer_class = Testimonial_patientSerializers
 
 #<<<<<<<<<<<<<<End Curd Testimonial_patient>>>>>>>>>>>
 
 #<<<<<<<<<<<<<<<Start Crud Room>>>>>>>>>>>>>>>>>>>>>
 class GetRoom(ListAPIView):
     queryset = Room.objects.all()
-    serializer_class = RoomSerializres
+    serializer_class = RoomSerializers
 
 class CreateRoom(ListCreateAPIView):
     queryset = Room.objects.all()
-    serializer_class = RoomSerializres
+    serializer_class = RoomSerializers
 
 class UpdateRoom(UpdateAPIView):
     queryset = Room.objects.all()
-    serializer_class = RoomSerializres
+    serializer_class = RoomSerializers
 
 class DeleteRoom(DestroyAPIView):
     queryset = Room.objects.all()
-    serializer_class = RoomSerializres
+    serializer_class = RoomSerializers
 
 #<<<<<<<<<<<<<<<End Crud Room>>>>>>>>>>>>>>>>>>>>>
 
@@ -110,117 +109,117 @@ class DeleteRoom(DestroyAPIView):
 
 class GetPatient(ListAPIView):
     queryset = Patient.objects.all()
-    serializer_class = PatientSerializres
+    serializer_class = PatientSerializers
 
 class CreatePatient(ListCreateAPIView):
     queryset = Patient.objects.all()
-    serializer_class = PatientSerializres
+    serializer_class = PatientSerializers
 
 class UpdatePatient(UpdateAPIView):
     queryset = Patient.objects.all()
-    serializer_class = PatientSerializres
+    serializer_class = PatientSerializers
 
 class DeletePatient(DestroyAPIView):
     queryset = Patient.objects.all()
-    serializer_class = PatientSerializres
+    serializer_class = PatientSerializers
 #<<<<<<<<<<<<<<<<<End Curd  Patient >>>>>>>>>>>>>>>>>
 
 #<<<<<<<<<<<<<<<<< Start Crud About_Hospital>>>>>>>>>>>>
 class GetAboutHospital(ListAPIView):
     queryset = About_hospital.objects.all()
-    serializer_class = About_hospitalSerializres
+    serializer_class = About_hospitalSerializers
 
 
 class CreateAboutHospital(ListCreateAPIView):
     queryset = About_hospital.objects.all()
-    serializer_class = About_hospitalSerializres
+    serializer_class = About_hospitalSerializers
 
 
 class UpdateAboutHospital(UpdateAPIView):
     queryset = About_hospital.objects.all()
-    serializer_class = About_hospitalSerializres
+    serializer_class = About_hospitalSerializers
 
 
 class DeleteAboutHospital(DestroyAPIView):
     queryset = About_hospital.objects.all()
-    serializer_class = About_hospitalSerializres
+    serializer_class = About_hospitalSerializers
 
 #<<<<<<<<<<<<<<<<< End Crud About_Hospital>>>>>>>>>>>>
 
 #<<<<<<<<<<<<<<<<<Start Crud Department>>>>>>>>>>>>>>>
 class GetDepartment(ListAPIView):
     queryset = Department.objects.all()
-    serializer_class = DepartmentSerializres
+    serializer_class = DepartmentSerializers
 
 
 class CreateDepartment(ListCreateAPIView):
     queryset = Department.objects.all()
-    serializer_class = DepartmentSerializres
+    serializer_class = DepartmentSerializers
 
 
 class UpdateDepartment(UpdateAPIView):
     queryset = Department.objects.all()
-    serializer_class = DepartmentSerializres
+    serializer_class = DepartmentSerializers
 
 
 class DeleteDepartment(DestroyAPIView):
     queryset = Department.objects.all()
-    serializer_class = DepartmentSerializres
+    serializer_class = DepartmentSerializers
 
 #<<<<<<<<<<<<<<<<<End Crud Department>>>>>>>>>>>>>>>
 
 #<<<<<<<<<<<<<<<<<Start Crud Operation>>>>>>>>>>>>>>
 class GetOperation(ListAPIView):
     queryset = Operation.objects.all()
-    serializer_class = OperationSerializres
+    serializer_class = OperationSerializers
 
 
 class CreateOperation(ListCreateAPIView):
     queryset = Operation.objects.all()
-    serializer_class = OperationSerializres
+    serializer_class = OperationSerializers
 
 
 class UpdateOperation(UpdateAPIView):
     queryset = Operation.objects.all()
-    serializer_class = OperationSerializres
+    serializer_class = OperationSerializers
 
 
 class DeleteOperation(DestroyAPIView):
     queryset = Operation.objects.all()
-    serializer_class = OperationSerializres
+    serializer_class = OperationSerializers
 
 #<<<<<<<<<<<<<<<<<End Crud Operation>>>>>>>>>>>>>>
 #<<<<<<<<<<<<<<<<Start Crud Equipment>>>>>>>>>>>>
 class GetEquipment(ListAPIView):
     queryset = Equipment.objects.all()
-    serializer_class = EmployeeSerializres
+    serializer_class = EquipmentSerializers
 
 
 class CreateEquipment(ListCreateAPIView):
     queryset = Equipment.objects.all()
-    serializer_class = EmployeeSerializres
+    serializer_class = EquipmentSerializers
 
 
 class UpdateEquipment(UpdateAPIView):
     queryset = Equipment.objects.all()
-    serializer_class = EmployeeSerializres
+    serializer_class = EquipmentSerializers
 
 
 class DeleteEquipment(DestroyAPIView):
     queryset = Equipment.objects.all()
-    serializer_class = EmployeeSerializres
+    serializer_class = EquipmentSerializers
 
 #<<<<<<<<<<<<<<<<End Crud Equipment>>>>>>>>>>>>
 
 #<<<<<<<<<<<<<<<<<Start Crud Cassa>>>>>>>>>>>>>>>
 class GetCassa(ListAPIView):
     queryset = Cassa.objects.all()
-    serializer_class = CassaSerializres
+    serializer_class = CassaSerializers
 
 
 class CreateCassa(ListCreateAPIView):
     queryset = Cassa.objects.all()
-    serializer_class = CassaSerializres
+    serializer_class = CassaSerializers
 
 
 #<<<<<<<<<<<<<<<<<End Crud Cassa>>>>>>>>>>>>>>>
