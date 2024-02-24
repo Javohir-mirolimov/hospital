960ffd11382fa034e6486b1d6de69607ecd1e015
Javohir-mirolimov <githup@gmail.com>
2024-02-24 15:04:10 +0500

    javoxir

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-
+from .views import *
 
 urlpatterns =[
+    #-----------------Start Filter Employee Model-------------------
+    path('filter_employee_by_fullname/', filter_employee_by_fullname)
 
 ]
--- main/views.py
+++ main/views.py
@@ -5,4 +5,20 @@ from .models import *
 from .serializers import *
 
 
+""" Start Filter Model """
+#<<<<<<<<<<<<<<<<<<Start Filter Epployee>>>>>>>>>>>>>>>>
 
+@api_view(['GET'])
+def filter_employee_by_fullname(request):
+    slug = request.GET.get('slug')
+    employee = Employee.objects.filter(slug__icontains=slug)
+    ser = EmployeeSerializers(employee, many=True)
+    return Response(ser.data)
+
+
+
+
+
+#<<<<<<<<<<<<<<<<<<End Filter Epployee>>>>>>>>>>>>>>>>
+
+""" End Filter Model """
